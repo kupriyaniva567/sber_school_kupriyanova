9d2841dfbc47ebc568ac40ad392d64e610b6bd11
OUT-Kupriyanova1-KV <karina.cooper567@gmail.com>
2022-05-19 21:00:56 +0300

    Сourse

---
+++ 000.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input)
+shape = str(input())
+if shape = 'треугольник':
+    print S=(p*((p-a)*(p-b)*(p-c)))**0.5
+elif shape = 'прямоугольник':
+    print(a*b)
+elif shape = 
