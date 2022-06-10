463b02ed98b5ba5c0c6f84950873df1d9747594b
OUT-Kupriyanova1-KV <karina.cooper567@gmail.com>
2022-06-10 16:31:50 +0300

    обновление

--- Block_2/part 2.4/task 1.py
+++ Block_2/part 2.4/task 1.py
@@ -1,4 +1,4 @@
 genome = input()
-a = genome.lower() # a.lower()-привод символа к нижнему регистру
+a = genome.lower() # привод символа к нижнему регистру
 print(((a.count('g')+a.count('c'))/len(genome))*100)
 
---
+++ Block_2/part 2.5/task 3.py
@@ -0,0 +1,4 @@
+a=[int(i) for i in input().split()]
+a.sort()
+m = []
+for i in range(len(a)):
