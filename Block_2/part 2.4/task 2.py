1080efb81636429136c6a08c44f8e48bcd6b4555
OUT-Kupriyanova1-KV <karina.cooper567@gmail.com>
2022-06-17 01:54:46 +0300

    обновление

--- Block_2/part 2.4/task 2.py
+++ Block_2/part 2.4/task 2.py
@@ -1,2 +1,14 @@
-a = input()
-t = a.lower()
+string = input() + " "
+i = 0
+count = 1
+new_string = ""
+
+while string[i] != " ":
+    if string[i] == string[i + 1]:
+        count += 1
+    else:
+        new_string += string[i] + str(count)
+        count = 1
+    i += 1
+
+print(new_string)
--- Block_2/part 2.5/task 3.py
+++ Block_2/part 2.5/task 3.py
@@ -1,4 +1,8 @@
 a=[int(i) for i in input().split()]
 a.sort()
 m = []
-for i in range(len(a)):
+for i in range(len(a)-1):
+    if a[i] == a[i+1]:
+      if a[i] not in m:
+        m.append(a[i])
+print(*m)
